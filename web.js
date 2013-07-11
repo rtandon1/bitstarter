53cf905cf35c2ea4cd2da3f621276db8e0f4d80e
Rishabh Tandon <rtandon@live.ca>
2013-07-11 20:33:53 +0000

    Update

--- web.js
+++ web.js
@@ -2,11 +2,19 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+function readFileSync_encoding(filename)
+{
+    var content = fs.readFileSync(filename);
+    
+    var buffer = new buffer(27)
+    return buffer.toString('utf8');
+}
+
 app.get('/', function(request, response) {
    var buffer = new Buffer(27);
-  //response.send('Hello World 2!');
-   buffer.write(fs.readFileSync('index.html'), "utf-8");
-   response.send(buffer.toString("utf-8", 0, 27);
+   response.send(readFileSync_encoding("index.html")
+    //buffer.write(fs.readFileSync('index.html'), "utf-8");
+   //response.send(buffer.toString("utf-8", 0, 27);
 });
 
 var port = process.env.PORT || 5000;
