31057b55f74a5ea8a132ad07161cc60c9e51ff05
Rishabh Tandon <rtandon@live.ca>
2013-07-11 20:14:47 +0000

    Update index.html

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+   var buffer = new Buffer(50);
+  //response.send('Hello World 2!');
+   buffer.write(fs.readFileSync(index.html, function(err, data)), "utf-8");
+   response.send(buffer.toString("utf-8", 0, 30);
 });
 
 var port = process.env.PORT || 5000;
